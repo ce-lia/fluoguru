878553dd7636acef88a6166a5401c561f7350f0b
cfranceschi-ledger <celia.franceschi-ext@ledger.fr>
2023-03-09 19:49:54 +0100

    update

--- _pages/about.md
+++ _pages/about.md
@@ -1,5 +1,5 @@
 ---
-title: About Hagura
+title: About Fluo Guru
 layout: post
 permalink: /about/
 published: true
@@ -7,8 +7,10 @@ published: true
 
 For more themes please visit [https://jekyll-themes.com](https://jekyll-themes.com){: target="_blank"}
 
+<!--
 Does the theme deserve a star?
 
-<!-- Place this tag where you want the button to render. -->
+Place this tag where you want the button to render.
 <a class="github-button" href="https://github.com/sharu725/hagura" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star sharu725/hagura on GitHub">Star</a>
 <script async defer src="https://buttons.github.io/buttons.js"></script>
+-->
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,21 +1,21 @@
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
 function theme() {
-     if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
+     if ( sessionStorage.getItem('bg') === 'rgb(242, 240, 227)') {
          
-            sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
+            sessionStorage.setItem('bg', 'rgb(68, 52, 79)');
+            sessionStorage.setItem('cc', '#F2F0E3');
             
          
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
+        sessionStorage.setItem('bg', 'rgb(68, 52, 79)');
+        sessionStorage.setItem('cc', '#F2F0E3');
         
     }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+    else if( sessionStorage.getItem('bg') === 'rgb(68, 52, 79)') {
         
-        sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
+        sessionStorage.setItem('bg', 'rgb(242, 240, 227)');
         sessionStorage.setItem('cc', '#333');
         
   
